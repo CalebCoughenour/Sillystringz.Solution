9e9f43f7ddcd2d755d1e188778af5e2f48e7bfaf
Caleb Coughenour <CalebCoughenour@protonmail.com>
2022-05-27 11:09:27 -0700

    remove tested routes, back to original state

--- Factory/Controllers/HomeController.cs
+++ Factory/Controllers/HomeController.cs
@@ -1,10 +1,10 @@
 using Microsoft.AspNetCore.Mvc;
+using Factory.Models;
 
 namespace Factory.Controllers
 {
     public class HomeController : Controller
-    {
-
+    {    
       [HttpGet("/")]
       public ActionResult Index()
       {
