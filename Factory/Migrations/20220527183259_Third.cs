ff90cc0b6f9b196fb9adc58de8b1027ec927d6ba
Caleb Coughenour <CalebCoughenour@protonmail.com>
2022-05-27 14:14:18 -0700

    add migrations to project file

---
+++ Factory/Migrations/20220527183259_Third.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Factory.Migrations
+{
+    public partial class Third : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "EngineerId",
+                table: "Machines",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EngineerId",
+                table: "Machines");
+        }
+    }
+}
---
+++ Factory/Migrations/20220527201910_Fourth.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Factory.Migrations
+{
+    public partial class Fourth : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EngineerId",
+                table: "Machines");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "EngineerId",
+                table: "Machines",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
