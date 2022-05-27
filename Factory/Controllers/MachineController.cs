7bdad378fd62050485e9211ae3eea3cf72f46559
Caleb Coughenour <CalebCoughenour@protonmail.com>
2022-05-27 13:39:03 -0700

    add details route

--- Factory/Controllers/MachineController.cs
+++ Factory/Controllers/MachineController.cs
@@ -22,5 +22,25 @@ namespace Factory.Controllers
       List<Machine> model = _db.Machines.OrderBy(model => model.Date).ToList();
       return View(model);
     }
+
+    public ActionResult Create()
+    {
+      return View();
+    }
+    [HttpPost]
+    public ActionResult Create(Machine machine)
+    {
+      _db.Machines.Add(machine);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+    public ActionResult Details (int id)
+    {
+      var thisMachine = _db.Machines
+        .Include(machine => machine.JoinEntities)
+        .ThenInclude(join => join.Engineer)
+        .FirstOrDefault(machine => machine.MachineId == id);
+      return View(thisMachine);
+    }
   }
 }
