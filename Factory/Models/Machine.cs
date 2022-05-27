1eb5cfb0710291d3d7ebd3eda256ab648aad9636
Caleb Coughenour <CalebCoughenour@protonmail.com>
2022-05-27 14:05:27 -0700

    remove unnecessary id for engineer

--- Factory/Models/Machine.cs
+++ Factory/Models/Machine.cs
@@ -1,21 +1,19 @@
 using System.Collections.Generic;
 
-
 namespace Factory.Models
 {
   public class Machine
   {
-    public Machine()
+    public Machine() 
     {
       this.JoinEntities = new HashSet<EngineerMachine>();
     }
 
-
+    public int MachineId { get; set; }
     public string Name { get; set; }
     public string Ticket { get; set; }
     public string Date { get; set; }
     public bool Completed { get; set; }
-    public int MachineId { get; set; }
-    public virtual ICollection<EngineerMachine> { get; }
+    public virtual ICollection<EngineerMachine> JoinEntities { get; }
   }
 }
